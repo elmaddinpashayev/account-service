62812117b021140decdb2aab7b78bf486d24c3c7
elmeddin123344 <33498115+elmeddin123344@users.noreply.github.com>
2023-04-09 14:46:13 +0400

    ss

--- src/main/java/com/haydikodlayalim/accountservice/config/CassandraConfiguration.java
+++ src/main/java/com/haydikodlayalim/accountservice/config/CassandraConfiguration.java
@@ -1,6 +1,7 @@
 package com.haydikodlayalim.accountservice.config;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.CqlSessionBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,20 +16,19 @@ import org.springframework.data.cassandra.core.convert.MappingCassandraConverter
 import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
-@EnableCassandraRepositories
+import java.net.InetSocketAddress;
+
+@EnableCassandraRepositories(sessionFactoryRef = "sessionFactory")
 @Configuration
 public class CassandraConfiguration extends AbstractCassandraConfiguration {
     @Value("${spring.data.cassandra.contact-points}")
     private String contactPoints;
 
-    @Value("${spring.data.cassandra.keyspace-name}")
-    private String keyspaceName;
-
-    @Value("${spring.data.cassandra.local-datacenter}")
-    private String localDatacenter;
-
     @Value("${spring.data.cassandra.port}")
     private int port;
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspaceName;
 
     @Value("${spring.data.cassandra.username}")
     private String username;
@@ -36,66 +36,35 @@ public class CassandraConfiguration extends AbstractCassandraConfiguration {
     @Value("${spring.data.cassandra.password}")
     private String password;
 
-    @Value("${spring.data.cassandra.schema-action}")
-    private String schemaAction;
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String localDatacenter;
 
     @Override
     protected String getKeyspaceName() {
         return keyspaceName;
     }
 
-    @Override
-    protected String getContactPoints() {
-        return contactPoints;
-    }
-
-    @Override
-    protected String getLocalDataCenter() {
-        return localDatacenter;
-    }
-
-    @Override
-    public String[] getEntityBasePackages() {
-        return new String[] {"com.haydikodlayalim.accountservice"};
-    }
-
-    @Override
-    public SchemaAction getSchemaAction() {
-        return SchemaAction.valueOf(schemaAction);
-    }
-
     @Bean
-    @Primary
-    public CqlSessionFactoryBean session() {
-        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
-        session.setContactPoints(getContactPoints());
-        session.setKeyspaceName(getKeyspaceName());
-        session.setLocalDatacenter(getLocalDataCenter());
-        session.setSchemaAction(getSchemaAction());
-        session.setUsername(username);
-        session.setPassword(password);
-        session.setPort(port);
-
-        return session;
+    public CqlSession session() {
+        CqlSessionBuilder builder = CqlSession.builder()
+                .withLocalDatacenter(localDatacenter)
+                .addContactPoint(new InetSocketAddress(contactPoints, port))
+                .withAuthCredentials(username, password);
+        return builder.build();
     }
 
-    @Bean
-    public CassandraConverter cassandraConverter() {
-        return new MappingCassandraConverter(cassandraMappingContext());
-    }
-
-    @Bean
-    public CassandraMappingContext cassandraMappingContext() {
-        return new CassandraMappingContext();
-    }
-    @Bean
-    public CassandraOperations cassandraTemplate(CqlSession session, CassandraConverter converter) {
-        return new CassandraAdminTemplate(session, converter);
-    }
-
-    @Bean
-    public CassandraConverter cassandraConverter(CassandraMappingContext mappingContext) {
-        return new MappingCassandraConverter(mappingContext);
-    }
-    
+//    @Bean
+//    public CassandraMappingContext cassandraMapping() {
+//        return new CassandraMappingContext();
+//    }
+//
+//    @Bean
+//    public CassandraConverter cassandraConverter() {
+//        return new MappingCassandraConverter(cassandraMapping());
+//    }
+//
+//    @Bean
+//    public CassandraAdminTemplate cassandraTemplate(CqlSession session, CassandraConverter cassandraConverter) {
+//        return new CassandraAdminTemplate(session, cassandraConverter);
+//    }
 }
